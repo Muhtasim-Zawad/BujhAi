0824fcb2540be7566ae3631bd875d20d91281cf2
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2026-07-10 23:40:37 +0600

    configured retro ui theme, fonts, and component setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import "@fontsource/archivo-black/400.css";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
 // import Canvas from "./components/layout/Canvas";
+import { ProjectCard } from "./components/layout/ProjectCard";
 
 function App() {
 	return (
-		<div className="flex flex-col h-screen w-screen bg-[#131314] overflow-hidden"></div>
+		// <div className="flex flex-col h-screen w-screen bg-[#131314] overflow-hidden">
+		<div>
+			<ProjectCard />
+		</div>
 	);
 }
 
