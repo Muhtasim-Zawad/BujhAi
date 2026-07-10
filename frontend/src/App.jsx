e4b6890c85b76638e1a9f0b1f963de1db31c2a58
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2026-07-10 22:30:05 +0600

    cleaned up unnecessary shadcn ui components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,8 @@
 // import Canvas from "./components/layout/Canvas";
-import InputDemo from "./components/layout/DemoConversation";
-import SourcesSidebar from "./components/layout/SourcesSidebar";
-import StudioSidebar from "./components/layout/StudioSidebar";
-import Navbar from "./components/layout/Navbar";
 
 function App() {
 	return (
-		<div className="flex flex-col h-screen w-screen bg-[#131314] overflow-hidden">
-			<Navbar />
-
-			{/* Sidebar and canvas content row */}
-			<div className="flex flex-1 p-3 gap-4 overflow-hidden box-border">
-				<SourcesSidebar />
-				<InputDemo />
-				<StudioSidebar />
-			</div>
-		</div>
+		<div className="flex flex-col h-screen w-screen bg-[#131314] overflow-hidden"></div>
 	);
 }
 
